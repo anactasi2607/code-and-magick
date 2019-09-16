47d1e5290699e8e3b224b9ee8b88a6dd7aee845c
anactasi2607 <anactasi2607@yandex.ru>
2019-09-16 18:27:13 +0300

    Продолжает работу по заданию, рефакторинг кода отрисовки баров

--- js/stat.js
+++ js/stat.js
@@ -2,6 +2,12 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var BAR_WIDTH = 40;
+var BAR_GAP = 50;
+var TEXT_GAP = 250;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -9,11 +15,11 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.strokeRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-window.renderStatistics = function (ctx) {
+window.renderStatistics = function (ctx, players) {
 
-  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, 100, 10, '#fff');
-  renderCloud(ctx, 100, 10);
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y);
 
   var canvas = document.getElementsByTagName('canvas')[0];
 
@@ -24,4 +30,28 @@ window.renderStatistics = function (ctx) {
   ctx.textBaseline = 'hanging';
   ctx.fillText('Ура, Вы победили!', 120, 30);
   ctx.fillText('Список результатов:', 120, 50);
+
+  /*  не могу понять, как высчитать высоту столбца и
+    координату 90 по игреку в отрисовке бара ctx.fillRect */
+
+  for (var i = 0; i < players.length; i++) {
+    ctx.fillStyle = '#000';
+    ctx.fillText(players[i], CLOUD_X + BAR_WIDTH + (BAR_GAP + BAR_WIDTH) * i, TEXT_GAP);
+
+    if (players[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      ctx.fillRect(CLOUD_X + BAR_WIDTH + (BAR_GAP + BAR_WIDTH) * i, 90, BAR_WIDTH, 150);
+    } else {
+      ctx.fillStyle = 'hsl(210, ' + Math.floor(Math.random() * 101) + '%, 50%)';
+      ctx.fillRect(CLOUD_X + BAR_WIDTH + (BAR_GAP + BAR_WIDTH) * i, 90, BAR_WIDTH, 150);
+    }
+  }
 };
+
+
+/*  for (var i = 0; i < players.length; i++) {
+    ctx.fillStyle = '#000';
+    ctx.fillText(players[i], CLOUD_X + BAR_WIDTH + (BAR_GAP + BAR_WIDTH) * i, TEXT_GAP);
+    ctx.fillStyle = 'hsl(210, ' + Math.floor(Math.random() * 101) + '%, 50%)';
+    ctx.fillRect(CLOUD_X + BAR_WIDTH + (BAR_GAP + BAR_WIDTH) * i, 90, BAR_WIDTH, 150);
+  }*/
