18c85c104d8ea35f42f87b342698cc2d07d26f06
anactasi2607 <anactasi2607@yandex.ru>
2019-09-17 13:51:56 +0300

    Дорабатывает скрипт до условий задания

--- js/stat.js
+++ js/stat.js
@@ -8,6 +8,9 @@ var GAP = 10;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
 var TEXT_GAP = 250;
+var BAR_HEIGHT = -150;
+// почему тут фиксированная высота, а если окно изменится по высоте?
+var BAR_FIRST_X = CLOUD_X + BAR_WIDTH;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -15,15 +18,23 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.strokeRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-window.renderStatistics = function (ctx, players) {
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+};
+
+window.renderStatistics = function (ctx, players, times) {
 
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
   renderCloud(ctx, CLOUD_X, CLOUD_Y);
-
-  var canvas = document.getElementsByTagName('canvas')[0];
-
-  var ctx = canvas.getContext('2d');
 
   ctx.font = '16px PT Mono';
   ctx.fillStyle = '#000';
@@ -31,27 +42,25 @@ window.renderStatistics = function (ctx, players) {
   ctx.fillText('Ура, Вы победили!', 120, 30);
   ctx.fillText('Список результатов:', 120, 50);
 
-  /*  не могу понять, как высчитать высоту столбца и
-    координату 90 по игреку в отрисовке бара ctx.fillRect */
+  var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
+
     ctx.fillStyle = '#000';
-    ctx.fillText(players[i], CLOUD_X + BAR_WIDTH + (BAR_GAP + BAR_WIDTH) * i, TEXT_GAP);
+    ctx.fillText(players[i], BAR_FIRST_X + (BAR_GAP + BAR_WIDTH) * i, TEXT_GAP);
+
+    var barHeight = (+BAR_HEIGHT * times[i]) / maxTime;
 
     if (players[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      ctx.fillRect(CLOUD_X + BAR_WIDTH + (BAR_GAP + BAR_WIDTH) * i, 90, BAR_WIDTH, 150);
     } else {
       ctx.fillStyle = 'hsl(210, ' + Math.floor(Math.random() * 101) + '%, 50%)';
-      ctx.fillRect(CLOUD_X + BAR_WIDTH + (BAR_GAP + BAR_WIDTH) * i, 90, BAR_WIDTH, 150);
     }
+
+    ctx.fillRect(BAR_FIRST_X + (BAR_GAP + BAR_WIDTH) * i, TEXT_GAP - GAP, BAR_WIDTH, barHeight);
+
+    ctx.font = '16px PT Mono';
+    ctx.fillStyle = '#000';
+    ctx.fillText(Math.round(times[i]), BAR_FIRST_X + (BAR_GAP + BAR_WIDTH) * i, barHeight + 220);
   }
 };
-
-
-/*  for (var i = 0; i < players.length; i++) {
-    ctx.fillStyle = '#000';
-    ctx.fillText(players[i], CLOUD_X + BAR_WIDTH + (BAR_GAP + BAR_WIDTH) * i, TEXT_GAP);
-    ctx.fillStyle = 'hsl(210, ' + Math.floor(Math.random() * 101) + '%, 50%)';
-    ctx.fillRect(CLOUD_X + BAR_WIDTH + (BAR_GAP + BAR_WIDTH) * i, 90, BAR_WIDTH, 150);
-  }*/
