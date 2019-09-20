14cde04afcd5e550f3d2c95c22ce27131385544c
anactasi2607 <anactasi2607@yandex.ru>
2019-09-20 18:34:28 +0300

    Вносит правки согласно замечаниям: циклы оформлены в функции, заменена функция-конструктор

--- js/setup.js
+++ js/setup.js
@@ -11,12 +11,6 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
 
-function GameWizard(n, c, e) {
-  this.name = n;
-  this.coatColor = c;
-  this.eyesColor = e;
-}
-
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
@@ -24,14 +18,26 @@ var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var wizards = [];
 
-for (i = 1; i <= numberWizard; i++) {
-  var wizardName = Math.floor(Math.random() * WIZARD_NAMES.length);
-  var wizardSurname = Math.floor(Math.random() * WIZARD_SURNAMES.length);
-  var wizardCoat = Math.floor(Math.random() * COAT_COLOR.length);
-  var wizardEyes = Math.floor(Math.random() * EYES_COLOR.length);
+var getRandomValue = function (arr) {
+  return Math.floor(Math.random() * arr.length);
+};
 
-  wizards[i] = new GameWizard(WIZARD_NAMES[wizardName] + ' ' + WIZARD_SURNAMES[wizardSurname], COAT_COLOR[wizardCoat], EYES_COLOR[wizardEyes]);
-}
+var createWizard = function (count) {
+  for (var i = 0; i <= count - 1; i++) {
+    var wizardName = getRandomValue(WIZARD_NAMES);
+    var wizardSurname = getRandomValue(WIZARD_SURNAMES);
+    var wizardCoat = getRandomValue(COAT_COLOR);
+    var wizardEyes = getRandomValue(EYES_COLOR);
+
+    wizards[i] = Object.create({
+      name: WIZARD_NAMES[wizardName] + ' ' + WIZARD_SURNAMES[wizardSurname],
+      coatColor: COAT_COLOR[wizardCoat],
+      eyesColor: EYES_COLOR[wizardEyes]
+    });
+  }
+};
+
+createWizard(numberWizard);
 
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
@@ -45,10 +51,14 @@ var renderWizard = function (wizard) {
 
 var fragment = document.createDocumentFragment();
 
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
-}
+var createFragment = function (arr) {
+  for (var i = 0; i < arr.length; i++) {
+    fragment.appendChild(renderWizard(arr[i]));
+  }
 
-similarListElement.appendChild(fragment);
+  similarListElement.appendChild(fragment);
+};
+
+createFragment(wizards);
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
